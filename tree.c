2da957cd2bf34c6bcd28e09b212832f2d23968a9
sopiU6 15 <sopiu615@MacBook-Air-de-sopiU6.local>
2017-07-03 17:24:15 +0200

    adding delete and add_son functions

--- tree.c
+++ tree.c
@@ -1,3 +1,30 @@
-#include "../h/tree.h"
+#include "tree.h"
+#include <stdlib.h>
 
+Tree *init_tree(int w, char e) {
+    Tree *node = (Tree*)malloc(sizeof(Tree*));
+    node->el = e;
+    node->weight = w;
+    node->left = NULL;
+    node->right = NULL;
+    
+    return node;
+}
 
+void add_son(Tree *father, char e, int w, int side) {
+    Tree *son = init_tree(w, e);
+    if (side < 0) {
+        father->left = son;
+    } else if (side > 0 ) {
+        father->right = son;
+    }
+
+}
+
+void destroy_tree(Tree *node) {
+    if (node != NULL) {
+        destroy_tree(node->left);
+        destroy_tree(node->right);
+        free(node);
+    }
+}
--- tree.h
+++ tree.h
@@ -1,9 +1,15 @@
+#ifndef TREE_H
+#define TREE_H
 typedef struct Tree Tree;
 
 struct Tree {
-    char el
+    char el;
     int weight;
     Tree *left;
     Tree *right;
 };
 
+Tree *init_tree(int, char);
+void add_son(Tree*, char, int, int);
+void destroy_tree(Tree*);
+#endif
