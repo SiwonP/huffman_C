1d139a289c423c897acd48839f1651fadb763b27
sopiU6 15 <nomisp96@hotmail.fr>
2017-07-11 10:25:10 +0200

    sorting out the segfault apparently

--- main.c
+++ main.c
@@ -9,7 +9,15 @@ int main(int argc, char *argv[]) {
     count_frequences(file, tab);
     TreeArray *array = init_tree_array();
 
+    make_basic_nodes(array, tab, 128);
+
     printf("%d", array->counter);
+
+    int i = 0;
+    while(array->nodes[i] != NULL) {
+        display_tree(array->nodes[i]);
+        i++;
+    }
 
     return 1;
 }
