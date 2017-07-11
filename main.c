f4982a77c34961544cf4a0729832d23799acf4cb
Simon Petit <nomisp96@hotmail.fr>
2017-07-11 19:33:31 +0200

    so that the executable works fine

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include "tree.h"
 #include "frequences.h"
 #include "compression.h"
+#include "dictionary.h"
 
 int main(int argc, char *argv[]) {
     FILE *file = fopen(argv[1], "r");
     int tab[128] = {0};
+    Entry dic[128];
 
     count_frequences(file, tab);
 
     Tree *tree = build_tree(tab, 128);
-    encode(tree,0,0);
+    encode(tree, dic, 0,0);
 
     display_tree(tree);
     destroy_tree(tree);
